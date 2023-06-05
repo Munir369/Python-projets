d076b62f86b78c150b7841e681951d680f96a5bd
Munir369 <135589912+Munir369@users.noreply.github.com>
2023-06-05 17:17:39 +0500

    Create QnA simple game.py
    this is based on the show kon bana ka  caroorpati ad game conntain questions which you have to answer

---
+++ QnA simple game.py
@@ -0,0 +1,54 @@
+import random
+print("HI! amita bacchan is taking from kon bana ga caroorpati")
+lstq = ["which is world's tallest building","who founded tesla","who is inventer of A.C current","Who give concept of black holes","Which theory repreasents the opposite of the black holes","How much humans know about the oceans in %","who was called queen of cities about 1000 to 1800 years ago"]
+lsta = ["Burj khalifa","Elon musk","Nikola tesla","Albert einstin","White holes","5%","constantinople"]
+a = random.randint(1,7)
+if a == 1:
+      print(lstq[0])
+      d = input("enter your answer : ")
+      if d == lsta[0]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 2:
+      print(lstq[1])
+      d = input("enter your number : ")
+      if d == lsta[1]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 3:
+      print(lstq[2])
+      d = input("enter your number : ")
+      if d == lsta[2]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 4:
+      print(lstq[3])
+      d = input("enter your number : ")
+      if d == lsta[3]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 5:
+      print(lstq[4])
+      d = input("enter your number : ")
+      if d == lsta[4]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 6:
+      print(lstq[5])
+      d = input("enter your number : ")
+      if d == lsta[5]:
+            print("you are correct")
+      else:
+            print("you are wrong")
+elif a == 7:
+      print(lstq[6])
+      d = input("enter your number : ")
+      if d == lsta[6]:
+            print("you are correct")
+      else:
+            print("you are wrong")
